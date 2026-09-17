61d76c189ec2a1f22dc119f48bf7e31984b9ada6
flagolos151461tc-cmd <f.lagolos.151461.tc@umindanao.edu.ph>
2026-09-17 22:34:25 +0800

    Update config for cloud environment variables

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "umtc",
-    "database": "studysync"
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "user": os.environ.get("DB_USER", "root"),
+    "password": os.environ.get("DB_PASSWORD", "your_local_password"),
+    "database": os.environ.get("DB_NAME", "studysync"),
+    "port": int(os.environ.get("DB_PORT", 3306))
 }
