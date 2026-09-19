c8daa2455536d5ba68be521119ee22081f1d5f98
flagolos151461tc-cmd <f.lagolos.151461.tc@umindanao.edu.ph>
2026-09-19 10:46:48 +0800

    modify the script

--- static/css/js/script.js
+++ static/css/js/script.js
@@ -1,11 +1,17 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+    if (registerBtn && container) {
+        registerBtn.addEventListener('click', () => {
+            container.classList.add("active");
+        });
+    }
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    if (loginBtn && container) {
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove("active");
+        });
+    }
 });
